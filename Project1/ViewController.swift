0022529d92163618c00655f5689de9bd4ace9bb0
Ogulcan Asa <ogulcanasa@hotmail.com>
2022-04-20 15:46:05 +0300

    Initial Commit

---
+++ Project1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Oğulcan Aşa on 20.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
